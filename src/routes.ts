2f1ec629de166626d9bb96a2992c94d054d42be5
Matheus Ishiyama <semrumo3@hotmail.com>
2021-04-20 11:06:29 -0300

    Aula 2

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { SettingsController } from './controller/SettingsController';
+const routes = Router();
+
+const settingsController = new SettingsController()
+
+routes.post("/settings", settingsController.create);
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Olá NLW5" });
-});
+app.use(express.json());
 
-app.post("/", (request, response) => {
-    return response.json({ message: "Usuário cadastrado" });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
